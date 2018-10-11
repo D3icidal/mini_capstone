b39833148316514370df06a4136cd5c561c9725d
Thomas Ng <D3icidal@gmail.com>
2018-10-11 19:46:25 -0400

    adds seeds.rb to add 2 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product1 = Product.new({ name:"iPhone", price:"1600", description:"iPhone 64gb XS15"})
+product2 = Product.new({ name:"TV", price:"600", description:"Vizio 4k TV"})
+
+product1.save
+product2.save
+
+puts Product.count
+p product1
