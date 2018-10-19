4506ac853f3cfbd9479b163fcdcb9f84b14307dc
Thomas Ng <D3icidal@gmail.com>
2018-10-18 21:27:06 -0400

    adds validation to product class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,43 @@
 class Product < ApplicationRecord
 
-#{}`is_discounted?` that returns true if an item is under $10 and false otherwise.
-def is_discounted?
-  price < 10 ? true : false
-end
+  # What should validations should product have?
 
-#{}`tax` which will return the tax that would be charged for a particular product. (Assume a 9% tax rate.)
-def tax
-  price * 0.09
-end
+  # name
+  #  presence
+  #  uniqueness
+  validates :name, :price, :description, presence: true 
 
 
-#{}`total` which will return the sum of the price + tax.
-def total
-  price+tax
-end
+  # price
+  #  presence
+  #  numericality
+  #  less than whatever you picked for your decimal
+  validates :price, numericality: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  
+
+
+  # description
+  #  at least 10 characters
+    #{}`is_discounted?` that returns true if an item is under $10 and false otherwise.
+
+
+  def is_discounted?
+    price < 10 ? true : false
+  end
+
+  #{}`tax` which will return the tax that would be charged for a particular product. (Assume a 9% tax rate.)
+  def tax
+    price * 0.09
+  end
+
+
+  #{}`total` which will return the sum of the price + tax.
+  def total
+    price+tax
+  end
+
+
+
 end
