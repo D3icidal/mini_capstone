0d5c5a40cc0ac8f3d86fd0f593e86aee4b43972f
Thomas Ng <D3icidal@gmail.com>
2018-10-24 20:37:33 -0400

    fixes validations with JWT - validation to server logs

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,18 +1,22 @@
 class Api::ProductsController < ApplicationController
   def index
-    @search_term = params[:search_term]
-    # p @search_term
-    # @products = Product.all
-    @products = Product.where('name LIKE ?', "%#{@search_term}%") 
-    @sort_by = "id => asc"
-    if params[:sort_by] == 'price'
-      # sort by price
-      @products = @products.order(:price => :asc)
-    else
-      # sort by id
-      @products = @products.order(:id => :asc)
-    end
-    render "index.json.jbuilder"
+      puts " \t****** Current user logged in: #{current_user.name}"
+      p 'current_user'
+      p current_user.name
+      p current_user #shows who is logged in (to verify authorization token)
+      @search_term = params[:search_term]
+      # p @search_term
+      # @products = Product.all
+      @products = Product.where('name LIKE ?', "%#{@search_term}%") 
+      @sort_by = "id => asc"
+      if params[:sort_by] == 'price'
+        # sort by price
+        @products = @products.order(:price => :asc)
+      else
+        # sort by id
+        @products = @products.order(:id => :asc)
+      end
+      render "index.json.jbuilder"
   end
 
   def create
