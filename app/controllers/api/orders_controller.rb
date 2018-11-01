5f0c2f255c09bd456bb7fd642954bef0aec8cfa4
Thomas Ng <D3icidal@gmail.com>
2018-10-31 22:30:26 -0400

    fixes idnex and starts order create for shopping cart

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -3,35 +3,40 @@ class Api::OrdersController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    # p current_user.name * 5
-    product = Product.find_by(id: params[:product_id])
-    quantity = params[:quantity].to_i
-    subtotal = 1.11
-    subtotal = product.price * quantity
-    tax = 1.11
-    tax = product.tax * quantity
-    total = product.total * quantity
-    # puts "\t\t\t subtotal is #{subtotal}\t***************"
-    @order = Order.new(
-      product_id: product.id,
-      quantity: quantity,
-      user_id: current_user.id,
-      subtotal: subtotal,
-      tax: tax,
-      total: total
-      )
-    if @order.save
-      # render json: "order created" * 5
-      puts "\t\torder created #{@order}"
-      # if current_user
-        render "show.json.jbuilder"
-      # else
-      #   render json: [], status: :unauthorized
-      # end
-    else
-      render json: "failed"
-      p "create failed - booo" * 5
-    end
+# 1)Find all of the current user’s carted products that have a status of “carted”.
+    carted = CartedProduct.where("status = 'carted' and user_id = #{current_user.id}")
+    render json: carted
+    # products = Product.find_by(id: params[:product_id])
+
+  ### Before shopping cart:
+    # product = Product.find_by(id: params[:product_id])
+    # quantity = params[:quantity].to_i
+    # subtotal = 1.11
+    # subtotal = product.price * quantity
+    # tax = 1.11
+    # tax = product.tax * quantity
+    # total = product.total * quantity
+    # # puts "\t\t\t subtotal is #{subtotal}\t***************"
+    # @order = Order.new(
+    #   product_id: product.id,
+    #   quantity: quantity,
+    #   user_id: current_user.id,
+    #   subtotal: subtotal,
+    #   tax: tax,
+    #   total: total
+    #   )
+    # if @order.save
+    #   # render json: "order created" * 5
+    #   puts "\t\torder created #{@order}"
+    #   # if current_user
+    #     render "show.json.jbuilder"
+    #   # else
+    #   #   render json: [], status: :unauthorized
+    #   # end
+    # else
+    #   render json: "failed"
+    #   p "create failed - booo" * 5
+    # end
   end
 
   def index
