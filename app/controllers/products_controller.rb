4e95cecc25e3da2f5e8b18f3270b88a4ecc13282
Thomas Ng <D3icidal@gmail.com>
2018-11-04 13:05:13 -0500

    adds select option dropdown for supplierID

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,30 @@ class ProductsController < ApplicationController
     # render json: "hello world"
     render "index.html.erb"
   end
+
+  def new #input form
+    # render json: "hellow world - new"
+    render "new.html.erb"
+  end
+
+  def show
+    # render json: "hello world -show"
+    @product = Product.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      supplier_id: params[:supplier_id]
+      )
+    p "NEW PRODUCT MADE WITH CREATE!!!!"
+    p @product
+    @product.save!
+    redirect_to "/products"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  #Products HTML
   get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  get '/products/:id' => 'products#show'
+  post '/products' => 'products#create'
 
   namespace :api do
     #index
